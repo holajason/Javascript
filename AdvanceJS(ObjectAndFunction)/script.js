ff7f744f6e7dc35bf21e26d139108a3a232584db
Jason <jasonlu@umich.edu>
2019-08-22 23:15:51 -0400

    Advance JS

---
+++ AdvanceJS(ObjectAndFunction)/script.js
@@ -0,0 +1,33 @@
+﻿//Function Constructor
+
+var john = {
+    name: john,
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+
+//Function contructor always start with a capital letter(P in this case)
+var Person = function (
+    name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+ 
+}
+
+Person.prototype.calcAge = function () {
+    console.log(2016 - this.yearOfBirth);
+}
+
+
+Person.prototype.lastName = 'Smith';
+
+//'new' create a new object
+var john = new Person('john', 1990, 'teacher');
+
+var jane = new Person('Jane', 1950, 'desinger');
+
+var mark = new Person('Mark', 1940, 'retired');
+john.calcAge();
+jane.calcAge();
+mark.calcAge();
